531abdefc9fb980d4025351cac20a948b192c973
Tom-Eric Gerritsen <ik@tom-eric.info>
2011-08-02 16:20:35 +0200

    Also include this middleware in the test environment

--- lib/vidibus-routing_error.rb
+++ lib/vidibus-routing_error.rb
@@ -3,8 +3,6 @@ require "routing_error/rack"
 
 module Vidibus::RoutingError
   class Engine < ::Rails::Engine
-    unless Rails.env.test?
-      config.app_middleware.insert_after("ActionDispatch::ShowExceptions", "Vidibus::RoutingError::Rack")
-    end
+    config.app_middleware.insert_after("ActionDispatch::ShowExceptions", "Vidibus::RoutingError::Rack")
   end
 end
